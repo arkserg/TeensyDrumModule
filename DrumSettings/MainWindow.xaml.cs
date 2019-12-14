be27e9faec55a314251808333ea0a0f56e19c6c9
Sergey Arkashin <s.arkashin@eastwind.ru>
2019-12-14 23:04:39 +0500

    Migrated to .Net Core Wpf application

--- DrumSettings/MainWindow.xaml.cs
+++ DrumSettings/MainWindow.xaml.cs
@@ -18,18 +18,18 @@ namespace Arkserg.TeensyDrumModule.DrumSettings
 
             Drums = new ObservableCollection<DrumPadViewModel>
             {
-                new SinglePiezoPadViewModel {Id = 0, Name = "Snare"},
-                new SinglePiezoPadViewModel {Id = 1, Name = "High Tom 1"},
-                new SinglePiezoPadViewModel {Id = 2, Name = "High Tom 2"},
-                new SinglePiezoPadViewModel {Id = 3, Name = "Floor Tom 1"},
-                new SinglePiezoPadViewModel {Id = 4, Name = "Floor Tom 2"},
-                new SinglePiezoPadViewModel {Id = 5, Name = "Bass"},
-                new SinglePiezoPadViewModel {Id = 6, Name = "CowBell"},
-                new YamahaPcyCymbalViewModel {Id = 7, Name = "HiHat"},
-                new YamahaPcyCymbalViewModel {Id = 8, Name = "Crash1"},
-                new YamahaPcyCymbalViewModel {Id = 9, Name = "Ride"},
-                new YamahaPcyCymbalViewModel {Id = 10, Name = "Crash2"},
-                new YamahaPcyCymbalViewModel {Id = 11, Name = "China"},
+                //new SinglePiezoPadViewModel {Channel = 0, Name = "Snare"},
+                //new DualZoneCymbalViewModel {Channel = 1, Name = "Crash"},
+                new ThreeZoneCymbalViewModel {Channel = 2, Name = "Ride"},
+                //new HiHatControllerViewModel {Channel = 3, Name = "HiHat Controller"},
+                new EmptyDrumPadViewModel {Channel = 4},
+                new EmptyDrumPadViewModel {Channel = 5},
+                new EmptyDrumPadViewModel {Channel = 6},
+                new EmptyDrumPadViewModel {Channel = 7},
+                new EmptyDrumPadViewModel {Channel = 8},
+                new EmptyDrumPadViewModel {Channel = 9},
+                new EmptyDrumPadViewModel {Channel = 10},
+                new EmptyDrumPadViewModel {Channel = 11},                
             };
 
             foreach (var drum in Drums)
@@ -53,8 +53,8 @@ namespace Arkserg.TeensyDrumModule.DrumSettings
             {
                 switch (drumPad)
                 {
-                    case YamahaPcyCymbalViewModel p:
-                        return element.FindResource("YamahaPcyTemplate") as DataTemplate;
+                    case ThreeZoneCymbalViewModel p:
+                        return element.FindResource("ThreeZoneCymbalTemplate") as DataTemplate;
                     case SinglePiezoPadViewModel p:
                         return element.FindResource("SinglePiezoPadTemplate") as DataTemplate;
                     default:
